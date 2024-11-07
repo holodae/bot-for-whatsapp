d2b8999b1c3f01d7620177e093c100588202fbd7
Baulybaev_pi330 <bqcrystal75@gmail.com>
2024-11-07 16:25:28 +0300

    🐛 fix: Был исправлен main.py

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 from flask import Flask
-
+from config.config import Config
+from database import init_db
 from src.auth.controllers.auth import AuthController 
+from src.user.controllers.user_controller import UserController
+
 
 class FlaskApp:
     def __init__(self):
         self.app = Flask(__name__)
-
+        self.app.config.from_object(Config)
+        init_db(self.app)
+        self.register_routes()
+        
+        
+    def register_routes(self):  
         self.authBp = AuthController().authBp
-        self.app.register_blueprint(self.authBp, url_prefix="/auth")
-
+        self.app.register_blueprint(AuthController().authBp, url_prefix="/auth")
+        self.app.register_blueprint(UserController().user_bp, url_prefix="/user")
+    
+    
     def start(self):
         self.app.run(debug=True)
 
